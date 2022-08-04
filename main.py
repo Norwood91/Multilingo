b5b93f2df3375430fb731cb530d9036b1a2327fc
Robert Norwood <you@example.com>
2022-08-04 14:16:39 -0700

    hardcoded text to the flash card image. This will be updated in a little bit.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter
 BACKGROUND_COLOR = "#B1DDC6"
+LANGUAGE_TEXT = ('Ariel', 40, 'italic')
+WORD_TEXT = ('Ariel', 70, 'bold')
 
 window = tkinter.Tk()
 window.title('Multilingo - Learn French The Easy Way')
@@ -8,7 +10,9 @@ window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 
 canvas = tkinter.Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 flash_card_front = tkinter.PhotoImage(file='./images/card_front.png')
-canvas.create_image(400, 275, image=flash_card_front )
+canvas.create_image(400, 275, image=flash_card_front)
+flash_card_language_text = canvas.create_text(380, 150, text='In English', fill='black', font=LANGUAGE_TEXT)
+flash_card_word_text = canvas.create_text(380, 293, text='Word', fill='black', font=WORD_TEXT)
 canvas.grid(column=0, row=0, columnspan=2)
 
 
